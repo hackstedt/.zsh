ef6b68c291929d9556366e28e861dc2951ccac54
Sven Mahlstedt <1940066+hackstedt@users.noreply.github.com>
2021-03-25 20:40:53 +0100

    Some Prompt updates / minor changes

--- rc/prompt.zsh
+++ rc/prompt.zsh
@@ -6,7 +6,6 @@ ENDSEC=0
 ZLE_RPROMPT_INDENT=-1
 
 precmd () {
-
   ENDSEC=$(date +%s)
   DIVSEC=$(expr $ENDSEC - $STARTSEC)
   PR_DIVSEC="$DIVSEC sec"
@@ -56,40 +55,36 @@ setprompt () {
   PR_NO_COLOUR="%{$terminfo[sgr0]%}"
 
   # See if we can use extended characters to look nicer.
-  typeset -A altchar
-  set -A altchar ${(s..)terminfo[acsc]}
-  PR_SET_CHARSET="%{$terminfo[enacs]%}"
-  PR_SHIFT_IN="%{$terminfo[smacs]%}"
-  PR_SHIFT_OUT="%{$terminfo[rmacs]%}"
-  PR_HBAR=${altchar[q]:--}
-  PR_ULCORNER=${altchar[l]:--}
-  PR_LLCORNER=${altchar[m]:--}
-  PR_LRCORNER=${altchar[j]:--}
-  PR_URCORNER=${altchar[k]:--}
-
-  if [[ "$TERM" == "linux" ]]; then
-    PR_HBAR=-
-    PR_ULCORNER=
-    PR_LLCORNER=
-    PR_LRCORNER=
-    PR_URCORNER=
-  fi
-
-  if [[ "$TERM" == "screen" ]]; then
-    PR_HBAR=-
-    PR_ULCORNER=--
-    PR_LLCORNER=--
-    PR_LRCORNER=--
-    PR_URCORNER=-
+  # UTF-8 Fixed
+  if [[ $(locale charmap) == "UTF-8" ]]; then
+    PR_SET_CHARSET=""
+    PR_SHIFT_IN=""
+    PR_SHIFT_OUT=""
+    PR_HBAR="─"
+    PR_ULCORNER="┌"
+    PR_LLCORNER="└"
+    PR_URCORNER="┐"
+    PR_LRCORNER="┘"
+  else
+    typeset -A altchar
+    set -A altchar ${(s..)terminfo[acsc]}
+    PR_SET_CHARSET="%{$terminfo[enacs]%}"
+    PR_SHIFT_IN="%{$terminfo[smacs]%}"
+    PR_SHIFT_OUT="%{$terminfo[rmacs]%}"
+    PR_HBAR='$PR_SHIFT_IN${altchar[q]:--}$PR_SHIFT_OUT'
+    PR_ULCORNER='$PR_SHIFT_IN${altchar[l]:--}$PR_SHIFT_OUT'
+    PR_LLCORNER='$PR_SHIFT_IN${altchar[m]:--}$PR_SHIFT_OUT'
+    PR_LRCORNER='$PR_SHIFT_IN${altchar[j]:--}$PR_SHIFT_OUT'
+    PR_URCORNER='$PR_SHIFT_IN${altchar[k]:--}$PR_SHIFT_OUT'
   fi
 
   # Decide if we need to set titlebar text.
   case $TERM in
     xterm*)
-      PR_TITLEBAR=$'%{\e]0;%(!.-=*[ROOT]*=- | .)%m:%~ | ${COLUMNS}x${LINES} | %y\a%}'
+      PR_TITLEBAR=$'%{\e]0;%(!.-=*[ROOT]*=- | .)%n@%m:%~ | ${COLUMNS}x${LINES} | %y\a%}'
       ;;
     screen)
-      PR_TITLEBAR=$'%{\e_screen \005 (\005t) | %(!.-=[ROOT]=- | .)%m:%~ | ${COLUMNS}x${LINES} | %y\e\\%}'
+      PR_TITLEBAR=$'%{\e_screen \005 (\005t) | %(!.-=[ROOT]=- | .)%n@%m:%~ | ${COLUMNS}x${LINES} | %y\e\\%}'
       ;;
     *)
       PR_TITLEBAR=''
@@ -108,9 +103,9 @@ setprompt () {
 $PR_RED$PR_SHIFT_IN$PR_ULCORNER$PR_BLUE$PR_HBAR$PR_SHIFT_OUT\
 {$PR_MAGENTA%$PR_PWDLEN<...<%~%<<$PR_BLUE}\
 $PR_SHIFT_IN$PR_HBAR$PR_RED$PR_HBAR${(e)PR_FILLBAR}$PR_BLUE$PR_HBAR$PR_SHIFT_OUT\
-{$PR_GREEN%(!.%SROOT%s.%m)$PR_GREEN:%l$PR_BLUE}\
+{$PR_GREEN%(!.%S%m%s.%m)$PR_GREEN:%l$PR_BLUE}\
 $PR_SHIFT_IN$PR_HBAR$PR_RED$PR_URCORNER$PR_SHIFT_OUT$PR_RED$PR_SHIFT_IN$PR_LLCORNER$PR_BLUE$PR_HBAR$PR_SHIFT_OUT\
-{%(?..$PR_LIGHT_RED%?$PR_BLUE:)${(e)PR_APM}$PR_YELLOW%D{%H:%M:%S}\
+{${(e)PR_APM}$PR_YELLOW%D{%H:%M:%S}%(?..|$PR_LIGHT_RED%?)\
 $PR_BLUE}$PR_SHIFT_IN$PR_HBAR$PR_SHIFT_OUT\
 $PR_NO_COLOUR'
 
